3e471d7cf76048e6c9e23c646c3db3af9d5b3079
Mario Vilches <110475661+mfvilches@users.noreply.github.com>
2024-10-15 00:20:39 +0000

    clase

--- src/app.py
+++ src/app.py
@@ -1,4 +1,23 @@
-from utils import db_connect
-engine = db_connect()
+from flask import Flask, request, render_template
+from pickle import load
 
-# your code here
+app = Flask(__name__)
+model = load(open("../models/random_forest_42.sav", "rb"))
+
+
+@app.route("/", methods = ["GET", "POST"])
+def index():
+    if request.method == "POST":
+        
+        # Obtain values from form
+        val1 = float(request.form["val1"])
+        val2 = float(request.form["val2"])
+        val3 = float(request.form["val3"])
+        
+        data = [[val1, val2, val3]]
+        print(data)
+        prediction = str(model.predict(data)[0])
+    else:
+        prediction = None
+    
+    return render_template("index.html", prediction = prediction)
